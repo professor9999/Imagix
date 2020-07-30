c0504859e656a2b314b6299e226f0f9d68fd2b2f
Kushal Jain <63453772+deadpoool69@users.noreply.github.com>
2020-07-31 00:38:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var http = require("http");
+var ejs = require("ejs");
+var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStratergy = require("passport-local");
+var session = require("express-session");
+//var Photographs = require("./models/photographs");
+//var Comment = require("./models/comments");
+//var User = require("./models/user");
+//var flash = require('connect-flash');
+
+mongoose.connect("mongodb://localhost/imagix", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//================
+//PRIMARY ROUTES
+//================
+
+app.use("/", function(req, res) {
+    res.send("Working");
+});
+
+app.listen(3000, "127.0.0.1", function() {
+    console.log("server running");
+});
